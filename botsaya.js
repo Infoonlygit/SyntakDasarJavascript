f67d826a46f5a680b00bceac6c4933b2d0a18e0c
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-20 16:28:15 +0700

    Update BelajarJs.js

--- botsaya.js
+++ botsaya.js
@@ -1,19 +1,5 @@
-const accountSid = 'YOUR_TWILIO_ACCOUNT_SID';
-const authToken = 'YOUR_TWILIO_AUTH_TOKEN';
-const client = require('twilio')(accountSid, authToken);
-
-const sendMessage = async (to, message) => {
-  try {
-    const result = await client.messages.create({
-      from: 'whatsapp:+6281535298570', // Twilio sandbox number
-      body: message,
-      to: `whatsapp:${to}`
-    });
-    console.log(`Message sent: ${result.sid}`);
-  } catch (error) {
-    console.error(`Error sending message: ${error.message}`);
-  }
-};
-
-// Example usage
-sendMessage('RECIPIENT_PHONE_NUMBER', 'Hello, selamat datang diwa bot');
+console.log(" _  __          _          ____                 
+| |/ /_____   _(_)_ __    / ___| __ _ _ __  ____
+| ' // _ \ \ / / | '_ \  | |  _ / _` | '_ \|_  /
+| . \  __/\ V /| | | | | | |_| | (_| | | | |/ / 
+|_|\_\___| \_/ |_|_| |_|  \____|\__,_|_| |_/___|")
