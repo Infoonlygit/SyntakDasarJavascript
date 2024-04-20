c9b6e3c8cb7576fb01ceba589afa13bcd3d0fe2c
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-20 21:03:48 +0700

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,4 +1,13 @@
 #!/usr/bin/javascript
 
-console.log("1. Hay")
-console.log("2. Hay")
+console.log("_ __ _ ____ 
+| |/ /_____ _(_)_ __ / ___| __ _ _ __ ____
+| ' // _ \ \ / / | '_ \ | | _ / _` | '_ \|_ /
+| . \ __/\ V /| | | | | | |_| | (_| | | | |/ / 
+|_|\_\___| \_/ |_|_| |_| \____|\__,_|_| |_/___|")
+
+console.log("Nama: Kevin Pandu Samudera")
+console.log("Kelas: 9.1")
+console.log("Usia: 15 tahun")
+console.log("Hobby: Coding")
+console.log("Alamat: Batam, Kep. Riau")
