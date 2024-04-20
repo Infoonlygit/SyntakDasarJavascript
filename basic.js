0fba069d1a93537da5bd10a1bbca2152ecf5e8ec
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-20 21:07:24 +0700

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,10 +1,6 @@
 #!/usr/bin/javascript
 
-console.log("_ __ _ ____ 
-| |/ /_____ _(_)_ __ / ___| __ _ _ __ ____
-| ' // _ \ \ / / | '_ \ | | _ / _` | '_ \|_ /
-| . \ __/\ V /| | | | | | |_| | (_| | | | |/ / 
-|_|\_\___| \_/ |_|_| |_| \____|\__,_|_| |_/___|")
+console.log("Selamat Datang Dibio Data Saya")
 
 console.log("Nama: Kevin Pandu Samudera")
 console.log("Kelas: 9.1")
